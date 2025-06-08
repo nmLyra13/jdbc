14494e830582a9ce9a98cb6f0d443d598879be18
Lyra <nmlyra13@gmail.com>
2025-06-08 20:37:04 -0300

    Update jdbc1-4 em 2025-06-08 20:37:04,87

--- jdbc6/src/application/Program.java
+++ jdbc6/src/application/Program.java
@@ -5,6 +5,7 @@ import java.sql.SQLException;
 import java.sql.Statement;
 
 import db.DB;
+import db.DbException;
 
 public class Program {
 
@@ -15,18 +16,27 @@ public class Program {
 
 		try {
 			conn = DB.getConnection();
+			conn.setAutoCommit(false); // does not confirm operations automatically
+
 			st = conn.createStatement();
-			int rows1 = st.executeUpdate("UPDATE seller SET BaseSAlary = 4090 " 
-			+ "WHERE DepartmentId = 3");
-			int rows2 = st.executeUpdate("UPDATE seller SET BaseSAlary = 5090 " 
-			+ "WHERE DepartmentId = 4");
-			
+
+			int rows1 = st.executeUpdate("UPDATE seller SET BaseSAlary = 800 " + "WHERE DepartmentId = 3");
+			int rows2 = st.executeUpdate("UPDATE seller SET BaseSAlary = 900 " + "WHERE DepartmentId = 4");
+
+			conn.commit(); // confirmation of operation without errors
+
 			System.out.println("rows1" + rows1);
 			System.out.println("rows2" + rows2);
-			
 
 		} catch (SQLException e) {
-			e.printStackTrace();
+			
+			try {
+				conn.rollback();
+				throw new DbException("Transation rolled back! Caused by: " + e.getMessage());
+			} catch (SQLException e1) {
+				throw new DbException("Error trying to rollback! Caused by: " + e.getMessage());
+			}
+
 		} finally {
 			DB.closeStatement(st);
 			DB.closeConnection();
