e26158119e6be1570a1501533be9a871100caef4
Lyra <nmlyra13@gmail.com>
2025-06-09 16:25:17 -0300

    atualiz..>

--- jdbc6/src/db/DbIntegrityException.java
+++ jdbc6/src/db/DbIntegrityException.java
@@ -7,5 +7,4 @@ public class DbIntegrityException extends RuntimeException{
 	public DbIntegrityException(String msg) {
 		super(msg);
 	}
-	
 }
--- jdbc6/src/db/Program.java
+++ jdbc6/src/db/Program.java
@@ -1,12 +1,9 @@
 package db;
 
-import java.sql.Connection;
-
 public class Program {
 
 	public static void main(String[] args) {
 
-		Connection conn = DB.getConnection();
 		DB.closeConnection();
 	}
 }
