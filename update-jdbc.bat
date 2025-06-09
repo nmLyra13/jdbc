2354ee7df7a37bcfb2dd47baee0e7c82ee81e3e7
Lyra <nmlyra13@gmail.com>
2025-06-08 21:26:46 -0300

    update 08/06/2025 21:26:46,51

--- update-jdbc.bat
+++ update-jdbc.bat
@@ -6,34 +6,8 @@ REM ==========================================================
 
 REM 1) Garante que estamos na pasta do reposit¢rio
 cd /d "C:\Projetos\java\jdbc"
-
-echo.
-echo === 1/4  Puxando o que h  no GitHub...
-git pull --rebase
-
-echo.
-echo === 2/4  Checando se h  mudan‡as locais...
-git diff-index --quiet HEAD --
-IF %ERRORLEVEL% EQU 0 (
-    echo Nenhuma alteracao detectada. Nada a fazer.
-    goto :EOF
-)
-
-echo.
-echo === 3/4  Adicionando alteracoes das pastas jdbc1..4
-git add jdbc*
-
-REM 4) Monta commit com timestamp YYYY-MM-DD_HHMMSS
-for /f "tokens=1-4 delims=/ " %%a in ("%date% %time%") do (
-    set "DIA=%%c-%%b-%%a"
-    set "HORA=%%d"
-)
-set "MSG=Update jdbc1-4 em %DIA% %HORA%"
-echo Commit: %MSG%
-git commit -m "%MSG%"
-
-echo.
-echo === 4/4  Enviando para origin/main...
+git add .
+git commit -m "update %date% %time%"
 git push origin main
 echo.
 echo Concluido!
